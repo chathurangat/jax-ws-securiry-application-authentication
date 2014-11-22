6ac1b38d3482928d67d920a9c57f1e8bad49337a
chathurangat <chathuranga.t@gmail.com>
2014-11-22 23:52:43 +0530

    intial app was set up

---
+++ src/main/java/com/chathurangaonline/jaxws/samples/impl/CalculatorServiceImpl.java
@@ -0,0 +1,44 @@
+package com.chathurangaonline.jaxws.samples.impl;
+
+import com.chathurangaonline.jaxws.samples.CalculatorService;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import java.util.List;
+import java.util.Map;
+
+
+@WebService
+public class CalculatorServiceImpl implements CalculatorService{
+
+    @Resource
+    private WebServiceContext webServiceContext;
+
+    @Override
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+
+    private boolean isAuthenticated(String username, String password){
+        if(username!=null && password!=null){
+            MessageContext messageContext = webServiceContext.getMessageContext();
+            Map httpHeaders  = (Map) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+
+            List usernameList  = (List) httpHeaders.get("username");
+            List passwordList = (List) httpHeaders.get("password");
+
+            if(usernameList.contains(username) && passwordList.contains(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
