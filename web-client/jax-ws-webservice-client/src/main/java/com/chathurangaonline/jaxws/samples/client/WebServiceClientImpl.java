42e7a6ced7807f73792c54efc6cbe7a81a685b80
chathurangat <chathuranga.t@gmail.com>
2014-11-26 18:17:09 +0530

    completed the web service client

--- web-client/jax-ws-webservice-client/src/main/java/com/chathurangaonline/jaxws/samples/client/WebServiceClientImpl.java
+++ web-client/jax-ws-webservice-client/src/main/java/com/chathurangaonline/jaxws/samples/client/WebServiceClientImpl.java
@@ -1,6 +1,12 @@
 package com.chathurangaonline.jaxws.samples.client;
 
 import com.chathurangaonline.jaxws.samples.impl.*;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class WebServiceClientImpl {
@@ -10,8 +16,15 @@ public class WebServiceClientImpl {
         CalculatorServiceImplService calculatorServiceImplService = new CalculatorServiceImplService();
         CalculatorServiceImpl calculatorService = calculatorServiceImplService.getCalculatorServiceImplPort();
 
-        double answer =  calculatorService.add(45,10);
+        Map<String, Object> req_ctx = ((BindingProvider)calculatorService).getRequestContext();
+        Map<String, List<String>> headers = new HashMap<String, List<String>>();
 
+        headers.put("Username", Collections.singletonList("chathuranga"));
+        headers.put("Password", Collections.singletonList("chathu@123"));
+        req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+
+        //in order to invoke the add method, you need to have valid login credentials 
+        double answer =  calculatorService.add(45,10);
         System.out.println(" answer is ["+answer+"]");
     }
 }
